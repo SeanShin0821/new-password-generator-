15938bb05e23d642a70f2797d139e28c68d3f8a5
SeanShin0821 <cshin4@gmu.edu>
2022-02-07 21:55:55 -0500

    index update

--- Develop/script.js
+++ Develop/script.js
@@ -5,6 +5,9 @@
 var generateBtn = document.querySelector("#generate");
 //Various elements for the password 
 var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 //define a function first 
 function generatePassword(){
     console.log("Welcome, you clicked!")
